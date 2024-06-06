d76fc1a9b911359585913891ec170e8f84395251
darcagn <darcagn@protonmail.com>
2024-06-05 21:39:16 -0600

    Update kos-info

--- kos-info.sh
+++ kos-info.sh
@@ -11,15 +11,23 @@ echo -e "KOS_PORTS\t   $(readlink -f ${KOS_PORTS})"
 echo -e "KOS_GCCVER\t   ${KOS_GCCVER}"
 echo -e "KOS_CHAIN\t   ${KOS_CHAIN}"
 echo -e "KOS_CHAIN_DIR\t   ${KOS_CHAIN_DIR}"
-echo -e "KOS_SH4_PRECISION  ${KOS_SH4_PRECISION}"
-echo -e "DC_ARM_BASE\t   ${DC_ARM_BASE}"
-echo -e "KOS_LOADER\t   ${KOS_LOADER}"
+if [ -f "${KOS_CHAIN_DIR}/build_date.txt" ]; then
+  echo -en "Toolchain build date: "
+  cat "${KOS_CHAIN_DIR}/build_date.txt"
+fi
 echo -e "KOS_CFLAGS\t  ${KOS_CFLAGS}"
 echo -e "KOS_LDFLAGS\t  ${KOS_LDFLAGS}"
 echo -e "KOS_AFLAGS\t  ${KOS_AFLAGS}"
 echo -e "KOS_CPPFLAGS\t ${KOS_CPPFLAGS}"
+echo -e "KOS_SH4_PRECISION  ${KOS_SH4_PRECISION}"
 if [[ $(readlink -f $(which sh-elf-gcc)) == ${KOS_CHAIN_DIR}/bin/sh-elf-gcc ]]; then
   echo "KOS environment compiler matches \$PATH: ✅"
 else
   echo "KOS environment compiler matches \$PATH: ❌"
+fi
+echo -e "KOS_LOADER\t   ${KOS_LOADER}"
+if [ -f "${DC_ARM_BASE}/bin/arm-eabi-gcc" ]; then
+  echo -e "DC_ARM_BASE\t   ${DC_ARM_BASE} [installed ✅]"
+else
+  echo -e "DC_ARM_BASE\t   ${DC_ARM_BASE} [installed ❌]"
 fi
